9f73113545d31d9b6cad609314101ca8e9aa167c
wijay58 <supun.wijemanna58@gmail.com>
2021-11-10 15:54:01 +0530

    initial

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer');
+const e = require('express');
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -8,11 +11,20 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse',upload.single('upfile'),function(req,res){
+  if(req.file) {
+    res.status(200).json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    })
+  } else {
+    res.status(400).json({error: "Please upload a file"})
+  }
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
